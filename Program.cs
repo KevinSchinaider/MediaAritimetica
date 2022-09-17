a676d1f075f6a21cf787ede8735561a21c8fef4d
KevinSchinaider <kevinschinaider05@outlook.com>
2022-09-16 22:35:02 -0300

    Solução do exercício

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Média Aritimética de 3 numeros\n");
+
+Console.Write("Digite seu primeiro número: ");
+double numero1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite seu segundo número: ");
+double numero2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite seu terceiro número: ");
+double numero3 = Convert.ToDouble(Console.ReadLine());
+
+double media = (numero1 + numero2 + numero3) / 3;
+
+Console.WriteLine($"\nMédia: {media:N1}");
